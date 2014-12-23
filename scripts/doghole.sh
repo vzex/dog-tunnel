4b3c5f6130ef1f8d1d093885f169ddb2626eef9e
ZhiFeng Hu <hufeng1987@gmail.com>
2014-12-23 20:52:20 +0800

    Modify dtunnel path to $HOME/bin/dtunnel

--- scripts/doghole.sh
+++ scripts/doghole.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# move dtunnel to /usr/bin/dtunnel
+# move dtunnel to $HOME/bin/dtunnel
 # echo "mypassword" > $HOME/.ssh/pw
 
 cat $HOME/.ssh/pw | sudo -S killall -9 dtunnel
@@ -8,8 +8,8 @@ sleep 2
 
 # the server side
 
-cat $HOME/.ssh/pw | sudo -S nice -n -10 /home/tomy/bin/dtunnel --reg node1.domain.com -local socks5 -clientkey verylongpasswd &
+cat $HOME/.ssh/pw | sudo -S nice -n -10 $HOME/bin/dtunnel --reg node1.domain.com -local socks5 -clientkey verylongpasswd &
 
 # the client side
 
-cat $HOME/.ssh/pw | sudo -S nice -n -10 /home/tomy/bin/dtunnel --link node1.domain.com -local :7070 -clientkey verylongpasswd &
+cat $HOME/.ssh/pw | sudo -S nice -n -10 $HOME/bin/dtunnel --link node1.domain.com -local :7070 -clientkey verylongpasswd &
