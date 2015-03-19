5b6d4650ece8484a834f106144ada24046df2969
ZhiFeng Hu <hufeng1987@gmail.com>
2015-03-19 09:40:09 +0800

    Update golang to 1.4.2 from 1.4.1
        Now golang 1.4.2 released , version bump.

--- scripts/install_linux.sh
+++ scripts/install_linux.sh
@@ -3,8 +3,8 @@
 cd ~
 yum install -y tar
 mkdir /root/goworkspace
-wget https://storage.googleapis.com/golang/go1.4.1.linux-amd64.tar.gz
-tar zxvf go1.4.1.linux-amd64.tar.gz
+wget https://storage.googleapis.com/golang/go1.4.2.linux-amd64.tar.gz
+tar zxvf go1.4.2.linux-amd64.tar.gz
 echo 'export GOROOT=/root/go' >> .bashrc
 echo 'export GOPATH=/root/goworkspace' >> .bashrc
 echo 'export PATH="/root/go/bin:/root/goworkspace/bin:"$PATH' >> .bashrc
