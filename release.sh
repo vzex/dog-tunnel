904ef8d4dacdaac442f7f22ce0cfa2b4bf611e6f
vzex <vzex@163.com>
2015-12-20 22:49:31 +0800

    release.sh support for go 1.5

--- release.sh
+++ release.sh
@@ -8,9 +8,9 @@ fi
 rm -f dtunnel_*$1_lite.tgz
 echo "Build ReleaseFile for version $version"
 
-cd $GOROOT/src
-GOOS=darwin GOARCH=amd64 sh ./make.bash
-cd -
+#cd $GOROOT/src
+#GOOS=darwin GOARCH=amd64 sh ./make.bash
+#cd -
 echo "build linux_amd64"
 GOOS=linux GOARCH=amd64 make
 tar zcvf dtunnel_linux_x64_$1_lite.tgz dtunnel_lite
@@ -23,9 +23,9 @@ tar zcvf dtunnel_mac_x64_$1_lite.tgz dtunnel_lite
 echo "build win32"
 GOOS=windows GOARCH=386 make && mv dtunnel_lite dtunnel_lite.exe
 tar zcvf dtunnel_win32_$1_lite.tgz dtunnel_lite.exe
-cd $GOROOT/src
-GOOS=linux GOARCH=arm sh ./make.bash
-cd -
+#cd $GOROOT/src
+#GOOS=linux GOARCH=arm sh ./make.bash
+#cd -
 echo "build linux_arm"
 GOOS=linux GOARCH=arm make
 tar zcvf dtunnel_linux_arm_$1_lite.tgz dtunnel_lite
