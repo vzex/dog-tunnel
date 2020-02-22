08054e8fc356fd1eef8f1622a204aa7e975de8f9
vzex <vzex@163.com>
2020-02-23 01:29:20 +0800

    update

--- src/pipe/mod.rs
+++ src/pipe/mod.rs
@@ -1,3 +1,4 @@
 pub mod pipe;
 pub mod tcp_pipe;
 pub mod udp_pipe;
+pub mod udp_punch_server;
--- src/pipe/udp_punch_server.rs
+++ src/pipe/udp_punch_server.rs
@@ -1,1 +1,37 @@
+use std::io;
+use tokio::net::TcpListener;
+use tokio::prelude::*;
 
+struct udp_punch_server;
+impl udp_punch_server {
+    async fn listen(&self, addr: &str) -> io::Result<()> {
+        let mut listener = TcpListener::bind(addr).await?;
+
+        loop {
+            let (mut socket, _) = listener.accept().await?;
+
+            tokio::spawn(async move {
+                let mut buf = [0; 1024];
+
+                // In a loop, read data from the socket and write the data back.
+                loop {
+                    let n = match socket.read(&mut buf).await {
+                        // socket closed
+                        Ok(n) if n == 0 => return,
+                        Ok(n) => n,
+                        Err(e) => {
+                            println!("failed to read from socket; err = {:?}", e);
+                            return;
+                        }
+                    };
+
+                    // Write the data back
+                    if let Err(e) = socket.write_all(&buf[0..n]).await {
+                        println!("failed to write to socket; err = {:?}", e);
+                        return;
+                    }
+                }
+            });
+        }
+    }
+}
